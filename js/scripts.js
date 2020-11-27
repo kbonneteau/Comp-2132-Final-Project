d30e5b20456a248b7edb6c23a9902c478d5e1726
kbonneteau <63267466+kbonneteau@users.noreply.github.com>
2020-11-26 20:10:35 -0800

    Player Object + Event Listener Setup

--- js/scripts.js
+++ js/scripts.js
@@ -27,18 +27,65 @@ class Dice {
             this.values[a] = b;
         }
         //return the randomly sorted array
-        return this.values;  
+        return this.values[0];  
 
     }
 
 };
 
-
+// test
 const diceTest = new Dice();
 diceTest.rollDice();
 popOutput.innerHTML = `<p> Test results: ${diceTest.describeSelf()}</p>`;
 
 
+// Player 1's dice
+const p1dice1 = new Dice();
+const p1dice2 = new Dice();
+
+// Player 2's dice
+const p2dice1 = new Dice();
+const p2dice2 = new Dice();
 
 
 
+// =======================================================
+// PLAYER OBJECT
+// =======================================================
+
+function Player(name){
+    this.name = name;
+    this.score = [];
+}
+Player.prototype.addToScore = function(score){
+    this.score = score;
+}
+Player.prototype.describeSelf = function(){
+    let description = `<p>${this.name}'s current dice roll is ${this.score}</p>`
+    
+    return description;
+}
+
+const player1 = new Player("Challenger");
+player1.addToScore(p1dice1.rollDice());
+
+
+// test
+popOutput.innerHTML = `<p> Test results: ${player1.describeSelf()}</p>`;
+
+
+
+// =======================================================
+// EVENT LISTENERS
+// =======================================================
+
+const btnRollDice = document.getElementById("btn-roll");
+const btnNewGame = document.getElementById("btn-new");
+
+btnNewGame.addEventListener("click",function(){
+    popOutput.innerHTML = `</p>You clicked New Game</p>`
+})
+
+btnRollDice.addEventListener("click",function(){
+    popOutput.innerHTML = `</p>You clicked Roll Dice</p>`
+})
