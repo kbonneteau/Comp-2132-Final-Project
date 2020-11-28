52d1dfe892a0d0a771425e1a2bf2435505c8c165
kbonneteau <63267466+kbonneteau@users.noreply.github.com>
2020-11-28 14:49:02 -0800

    Gameplay logic added.
    Added logic for gameplay.
    
    - Calculating the round
    - Calculating the total
    - Displaying results
    - Prevent default behaviour of button after 3 rounds

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,7 @@ const p1RoundScore = document.getElementById("p1-round");
 const p1TotalScore = document.getElementById("p1-total");
 const p2RoundScore = document.getElementById("p2-round");
 const p2TotalScore = document.getElementById("p2-total");
+const btnRollDice = document.getElementById("btn-roll");
 
 
 // =======================================================
@@ -70,9 +71,6 @@ Player.prototype.addDice = function(dice1, dice2){
     this.die.push(dice1);
     this.die.push(dice2);
 }
-// Player.prototype.stateDice = function(){
-//     return this.die;
-// }
 Player.prototype.addToScore = function(score){
     this.score = score;
 }
@@ -80,37 +78,18 @@ Player.prototype.describeSelf = function(){
     let description = `<p>${this.name}'s current dice roll is ${this.die[0]} and ${this.die[1]}.<br>Current score: ${this.score}</p>`
     
     return description;
-}
-// Player.prototype.displayScore = function(){
-//     return this.score;
-// }
-
-
-// Instantiate two players. Add dice to their possession.
+};
 
 
 const player1 = new Player("Challenger");
-p1dice1.rollDice();
-p1dice2.rollDice();
-player1.addDice(p1dice1.describeSelf(),p1dice2.describeSelf());
-player1.addToScore(calculateRoundScore(p1dice1.describeSelf(),p1dice2.describeSelf()));
 
 const player2 = new Player("Opponent");
-// p2dice1.rollDice();
-// p2dice2.rollDice();
-player2.addDice(p2dice1.describeSelf(),p2dice2.describeSelf());
-
 
 // player1.addToScore(p1dice1.rollDice());
 
 
 // test
-popOutput.innerHTML = `<p> Test results: ${player1.describeSelf()}<br>${player2.describeSelf()}</p>`;
-
-
-
-
-
+// popOutput.innerHTML = `<p> Test results: ${player1.describeSelf()}<br>${player2.describeSelf()}</p>`;
 
 
 
@@ -118,20 +97,11 @@ popOutput.innerHTML = `<p> Test results: ${player1.describeSelf()}<br>${player2.
 // EVENT LISTENERS
 // =======================================================
 
-const btnRollDice = document.getElementById("btn-roll");
 const btnNewGame = document.getElementById("btn-new");
-let currentRoundScore;
 
 btnNewGame.addEventListener("click",function(){
     popOutput.innerHTML = `</p>You clicked New Game</p>`
 });
-
-btnRollDice.addEventListener("click",function(){
-    popOutput.innerHTML = `</p>You clicked Roll Dice</p>`
-});
-
-
-
 
 
 
@@ -139,6 +109,62 @@ btnRollDice.addEventListener("click",function(){
 // GAMEPLAY LOGIC
 // =======================================================
 
+let numRoundsPlayed = 0;
+
+let p1CurrentRoundScore;
+let p1GameScore = 0;
+
+let p2CurrentRoundScore;
+let p2GameScore = 0;
+
+
+btnRollDice.addEventListener("click",function(e){
+    
+    if (numRoundsPlayed > 2){
+        e.preventDefault();
+    }else{
+        playGame();
+        numRoundsPlayed++;
+        console.log(numRoundsPlayed);
+    }
+
+});
+
+
+
+// =======================================================
+// FUNCTIONS
+// =======================================================
+
+
+
+function playGame(){
+
+    // Player 1 rolls
+    p1dice1.rollDice();
+    p1dice2.rollDice();
+
+    p1CurrentRoundScore = calculateRoundScore(p1dice1.describeSelf(),p1dice2.describeSelf());
+
+    p1RoundScore.innerHTML = `${p1CurrentRoundScore}`
+
+    p1GameScore = calculateTotalScore(p1GameScore,p1CurrentRoundScore);
+
+    p1TotalScore.innerHTML = `${p1GameScore}`
+
+    // Player 2 rolls
+    p2dice1.rollDice();
+    p2dice2.rollDice();
+
+    p2CurrentRoundScore = calculateRoundScore(p2dice1.describeSelf(),p2dice2.describeSelf());
+
+    p2RoundScore.innerHTML = `${p2CurrentRoundScore}`
+
+    p2GameScore = calculateTotalScore(p2GameScore,p2CurrentRoundScore);
+
+    p2TotalScore.innerHTML = `${p2GameScore}`
+    
+};
 
 function calculateRoundScore(dice1, dice2){
     
@@ -155,27 +181,9 @@ function calculateRoundScore(dice1, dice2){
     return roundScore;
 };
 
+function calculateTotalScore(totalScore,roundScore){    
+    totalScore = +totalScore + +roundScore;
+    
+    return totalScore;
+};
 
-
-/*
-
-function
-
-math function for adding up calculateRoundScore 
-
-if dice1 = 1 OR dice2 = 1
-    then Score = 0
-else if dice1 === dice2
-    then Score = (dice1.value+dice2.value)*2
-else (
-    Score = dice1.value+dice2.value
-)
-
-return Score
-
-
-
-
-currentRoundScore = calculateRoundScore();
-
-*/
